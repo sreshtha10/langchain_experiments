59008b65e95501bdfce875a22b5b4d4c8596e66c
Sreshtha Mehrotra <sreshtha.mehrotra@gmail.com>
2025-05-10 03:41:13 +0530

    added readme, travel guide using LCEL

---
+++ Readme.md
@@ -0,0 +1,12 @@
+## Experimenting with the following topics related to LLM and Langchains:
+
+1. Using OpenAI via Langchain
+2. Using Langchain community to use OpenSource Models.
+3. Prompt Templates
+4. LCEL and Chains
+5. Mainting Chat History.
+6. Embeddings and Similarity Finder
+7. Vector Stores
+8. RAG
+9. Image Processing
+10. Agents
---
+++ src/travel_guide_using_chains.py
@@ -0,0 +1,30 @@
+from langchain_community.chat_models import ChatOllama
+from langchain.prompts import PromptTemplate
+import streamlit as st
+
+
+
+llm = ChatOllama(model="llama3.2:3b")
+
+prompt = PromptTemplate(
+    input_variables=["place","month","days"],
+    template="""
+    Create a travel itinerary for {place} for {days} days. What are the must visit attractions during {month} and list some useful phrases in their local language.
+    """
+)
+
+st.title("Ask Our Trip Planner!")
+
+place = st.text_input("Enter the place you want to visit")
+month = st.text_input("When are planning your trip?")
+days = st.number_input("For how long are you visiting?", min_value=1)
+
+chain = prompt | llm
+
+if place and month and days:
+    response = chain.invoke({
+        "place":place,
+        "month":month,
+        "days":days
+    })
+    st.write(response.content)
