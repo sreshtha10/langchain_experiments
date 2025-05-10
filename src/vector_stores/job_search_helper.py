8dc74f8ac0c5e14f38817c565589534a23221dea
Sreshtha Mehrotra <sreshtha.mehrotra@gmail.com>
2025-05-11 05:27:36 +0530

    impl job similarity search using Chroma

---
+++ src/vector_stores/job_search_helper.py
@@ -0,0 +1,22 @@
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+
+
+llm = OllamaEmbeddings(model="nomic-embed-text")
+
+document = TextLoader("src/utils/job_listings.txt",encoding="utf-8").load()
+
+text_splitters = RecursiveCharacterTextSplitter(chunk_size=200,chunk_overlap=10)
+
+chunks = text_splitters.split_documents(documents=document)
+
+db = Chroma.from_documents(chunks,llm)
+
+user_input = input("Enter the query")
+embedding_vector = llm.embed_query(user_input)
+
+docs = db.similarity_search_by_vector(embedding_vector)
+
+print(docs)
